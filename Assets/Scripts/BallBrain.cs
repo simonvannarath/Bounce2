b1fe31afd31d1d3faba1094c55a51235c5f5c3e9
simonvannarath <vvidfinishing@gmail.com>
2017-08-15 15:06:11 +1000

    Initial upload of classwork

---
+++ Assets/Scripts/BallBrain.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBrain : MonoBehaviour {
+
+    Rigidbody rigi;
+	// Use this for initialization
+	void Start ()
+    {
+        rigi = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "Bounce")
+        print("Hello!");
+        rigi.velocity = (transform.position - collision.contacts[0].point);
+    }
+}
---
+++ Assets/Scripts/Planetoid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planetoid : MonoBehaviour {
+
+    Rigidbody rigi;
+    Transform sun;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigi = GetComponent<Rigidbody>();
+        sun = GameObject.Find("Sun").transform;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        rigi.AddForce(sun.position - transform.position);
+
+	}
+}
